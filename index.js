e6f2ab053b61bb386e06206979d8d308237a81f1
Nidhish-Agarwal <nidhishagarwal14@gmail.com>
2025-01-06 12:47:24 +0530

    Completed the Assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const express = require('express');
-const { resolve } = require('path');
+const express = require("express");
+const { resolve } = require("path");
+const mongoose = require("mongoose");
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "./.env",
+  });
+}
 
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+mongoose
+  .connect(process.env.DB_URL)
+  .then((data) =>
+    console.log("DataBase connected Sucessfully", data.connection.host)
+  )
+  .catch((err) => console.log(err.message));
+
+app.get("/", (req, res) => {
+  res.sendFile(resolve(__dirname, "pages/index.html"));
 });
 
 app.listen(port, () => {
